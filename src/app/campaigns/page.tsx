a49a0e1fde76e5bd99f9cff774d36af5ee98240a
SabitCanTurunc <turuncsc@gmail.com>
2024-12-22 08:29:30 +0300

    campaigns background

--- src/app/campaigns/page.tsx
+++ src/app/campaigns/page.tsx
@@ -1,11 +1,23 @@
 "use client"
 import { Campaigns } from '@/components/campaigns/campaigns'
 import React from 'react'
+import Image from 'next/image'
 
 const CampaignsPage = () => {
   return (
     <div>
-      <Campaigns/>
+      <Image
+        src="/images/backgroundCampaigns.gif"
+        alt="background"
+        layout="fill"
+        objectFit="cover"
+        priority
+        className="relative w-full h-screen absolute inset-0 z-[-1]"
+      />
+      <div className='px-5'>
+        <Campaigns />
+
+      </div>
     </div>
   )
 }
