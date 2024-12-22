96e235cc803c9212f05c5a12baee2ef4a94c9288
SabitCanTurunc <turuncsc@gmail.com>
2024-12-22 08:29:55 +0300

    my campaigns operations

--- src/components/campaigns/my-campaigns.tsx
+++ src/components/campaigns/my-campaigns.tsx
@@ -21,13 +21,15 @@ import { Progress } from '../ui/progress';
 import React from 'react';
 import { Separator } from '../ui/separator';
 import { ScrollArea } from '../ui/scroll-area';
-import { addTier } from '@/hooks/use-campaign-operations';
+import { addTier, removeTier, useCampaignStatus, useCampaignTiers } from '@/hooks/use-campaign-operations';
 import { Address } from 'viem';
 import { string } from 'zod';
 import { Menubar, MenubarContent, MenubarItem, MenubarMenu, MenubarTrigger } from '@radix-ui/react-menubar';
 import { MenubarSeparator } from '../ui/menubar';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
+import { CampaignTiers } from './campaignTiers';
+import CampaignStatusUi from './campaignStatusUi';
 
 export const MyCampaigns = () => {
   const { address, isConnected } = useAccount(); // Use inside the component
@@ -38,6 +40,9 @@ export const MyCampaigns = () => {
 
   const [tierName, setTierName] = React.useState("");
   const [tierAmount, setTierAmount] = React.useState(BigInt(""));
+
+  const [tierIndex, setTierIndex] = React.useState(BigInt(""));
+
 
   const router = useRouter()
 
@@ -94,6 +99,15 @@ export const MyCampaigns = () => {
     }
   }
 
+  const handleDelete = (campaignAddress: Address) => {
+    try {
+      const result = removeTier(campaignAddress, tierIndex)
+    } catch {
+
+    }
+  }
+
+
   return (
     <div className="flex flex-col justify-center items-center gap-4">
       <div className=" flex flex-col gap-4 w-full items-center">
@@ -131,132 +145,130 @@ export const MyCampaigns = () => {
                   </ul>
                 </div>
               </CardContent>
+
               <CardFooter>
+
 
                 <Menubar>
                   <MenubarMenu>
-                    <MenubarTrigger>Edit Campaign</MenubarTrigger>
-                    <MenubarContent>
-                      <MenubarItem>
+                    <MenubarTrigger><Button>Edit Campaign</Button></MenubarTrigger>
 
-                        <Dialog>
-                          <DialogTrigger asChild>
-                            <Button variant="outline" onClick={(e) => e.stopPropagation()}>Add Tier</Button>
-                          </DialogTrigger>
-                          <DialogContent className="sm:max-w-[425px]">
-                            <DialogHeader>
-                              <DialogTitle>Edit campaign</DialogTitle>
-                              <DialogDescription>
-                                Make changes to your profile here. Click save when you're done.
-                              </DialogDescription>
-                            </DialogHeader>
+                    <MenubarContent >
+                      <div className='border-2 rounded-lg border-red-700 bg-red-700 flex flex-col items-center justify-center py-2'>
+
+                        <MenubarItem>
+
+                          <Dialog>
+                            <DialogTrigger asChild>
+                              <Button variant="outline" onClick={(e) => e.stopPropagation()}>Add Tier</Button>
+                            </DialogTrigger>
+                            <DialogContent className="sm:max-w-[425px]" onClick={(e) => e.stopPropagation()}>
+                              <DialogHeader>
+                                <DialogTitle>Edit campaign</DialogTitle>
+                                <DialogDescription>
+                                  Make changes to your profile here. Click save when you're done.
+                                </DialogDescription>
+                              </DialogHeader>
 
 
-                            <div className="grid gap-4 py-4">
-                              <div className="grid grid-cols-4 items-center gap-4">
-                                <Label htmlFor="name" className="text-right">
-                                  Tier
-                                </Label>
-                                <Input
-                                  id="name"
-                                  value={tierName}
-                                  onChange={(e) => setTierName(e.target.value)}
-                                  className="col-span-3"
-                                />
+                              <div className="grid gap-4 py-4">
+                                <div className="grid grid-cols-4 items-center gap-4">
+                                  <Label htmlFor="name" className="text-right">
+                                    Tier
+                                  </Label>
+                                  <Input
+                                    id="name"
+                                    value={tierName}
+                                    onChange={(e) => {
+                                      e.stopPropagation(); // Olay yayılmasını durdur
+                                      setTierName(e.target.value);
+                                    }}
+                                    className="col-span-3"
+                                  />
+
+                                </div>
+                                <div className="grid grid-cols-4 items-center gap-4">
+                                  <Label htmlFor="amount" className="text-right">
+                                    Amount (WEI)
+                                  </Label>
+                                  <Input
+                                    id="amount"
+                                    value={tierAmount.toString()}
+                                    onChange={(e) => {
+                                      e.stopPropagation(); // Olay yayılmasını durdur
+                                      try {
+                                        setTierAmount(BigInt(e.target.value));
+                                      } catch {
+                                        setTierAmount(BigInt(0));
+                                      }
+                                    }}
+                                    className="col-span-3"
+                                  />
+
+                                </div>
                               </div>
-                              <div className="grid grid-cols-4 items-center gap-4">
-                                <Label htmlFor="amount" className="text-right">
-                                  Amount (WEI)
-                                </Label>
-                                <Input
-                                  id="amount"
-                                  value={tierAmount.toString()}
-                                  onChange={(e) => {
-                                    try {
-                                      setTierAmount(BigInt(e.target.value));
-                                    } catch {
-                                      setTierAmount(BigInt(0));
-                                    }
-                                  }}
-                                  className="col-span-3"
-                                />
-
-                              </div>
-                            </div>
 
 
-                            <DialogFooter>
-                              <Button type="submit" onClick={() => handleSubmit(campaign.campaignAddress)}>Save changes</Button>
-                            </DialogFooter>
-                          </DialogContent>
-                        </Dialog>
+                              <DialogFooter>
+                                <Button type="submit" onClick={() => handleSubmit(campaign.campaignAddress)}>Save changes</Button>
+                              </DialogFooter>
+                            </DialogContent>
+                          </Dialog>
 
-                      </MenubarItem>
-                      <MenubarSeparator />
+                        </MenubarItem>
 
-                      <MenubarItem>
-                        <Button>
-                          see tiers
+                        <MenubarSeparator />
 
-                        </Button>
-                      </MenubarItem>
+                        <MenubarItem>
 
+                          <Dialog>
+                            <DialogTrigger asChild>
+                              <Button variant="outline" onClick={(e) => e.stopPropagation()}>Delete Tier</Button>
+                            </DialogTrigger>
+                            <DialogContent onClick={(e) => e.stopPropagation()} className="sm:max-w-[425px]">
+                              <DialogHeader>
+                                <DialogTitle>Delete Tier</DialogTitle>
+                                <DialogDescription>
+                                  Select a tier for delete. Click save when you're done.
+                                </DialogDescription>
+                              </DialogHeader>
+
+
+
+                              <CampaignTiers campaignAddress={campaign.campaignAddress} />
+
+
+
+                              <Input
+                                id="name"
+                                value={tierIndex.toString()}
+                                onChange={(e) => {
+                                  try {
+                                    setTierIndex(BigInt(e.target.value));
+                                  } catch {
+                                    setTierIndex(BigInt(0));
+                                  }
+                                }}
+                                className="col-span-3"
+                              />
+
+
+
+                              <DialogFooter>
+                                <Button type="submit" onClick={() => handleDelete(campaign.campaignAddress)}>Delete tier</Button>
+                              </DialogFooter>
+                            </DialogContent>
+                          </Dialog>
+
+                        </MenubarItem>
+                      </div>
                     </MenubarContent>
+
 
                   </MenubarMenu>
                 </Menubar>
 
-                {/* <Dialog>
-                  <DialogTrigger asChild>
-                    <Button variant="outline">Add Tier</Button>
-                  </DialogTrigger>
-                  <DialogContent className="sm:max-w-[425px]">
-                    <DialogHeader>
-                      <DialogTitle>Edit campaign</DialogTitle>
-                      <DialogDescription>
-                        Make changes to your profile here. Click save when you're done.
-                      </DialogDescription>
-                    </DialogHeader>
-
-
-                    <div className="grid gap-4 py-4">
-                      <div className="grid grid-cols-4 items-center gap-4">
-                        <Label htmlFor="name" className="text-right">
-                          Tier
-                        </Label>
-                        <Input
-                          id="name"
-                          value={tierName}
-                          onChange={(e) => setTierName(e.target.value)}
-                          className="col-span-3"
-                        />
-                      </div>
-                      <div className="grid grid-cols-4 items-center gap-4">
-                        <Label htmlFor="amount" className="text-right">
-                          Amount (WEI)
-                        </Label>
-                        <Input
-                          id="amount"
-                          value={tierAmount.toString()}
-                          onChange={(e) => {
-                            try {
-                              setTierAmount(BigInt(e.target.value));
-                            } catch {
-                              setTierAmount(BigInt(0));
-                            }
-                          }}
-                          className="col-span-3"
-                        />
-
-                      </div>
-                    </div>
-
-
-                    <DialogFooter>
-                      <Button type="submit" onClick={() => handleSubmit(campaign.campaignAddress)}>Save changes</Button>
-                    </DialogFooter>
-                  </DialogContent>
-                </Dialog> */}
+                <CampaignStatusUi campaignAddress={campaign.campaignAddress} />
 
               </CardFooter>
             </Card>
